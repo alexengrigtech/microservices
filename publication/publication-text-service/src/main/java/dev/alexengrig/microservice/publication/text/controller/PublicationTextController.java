0c6fa9a84c49c360932c35b1b69bc3cdfca7239f
Grig Alex <alexengrigdev@gmail.com>
2023-01-06 06:20:38 +0300

    Fix PublicationTextController#publish

--- publication/publication-text-service/src/main/java/dev/alexengrig/microservice/publication/text/controller/PublicationTextController.java
+++ publication/publication-text-service/src/main/java/dev/alexengrig/microservice/publication/text/controller/PublicationTextController.java
@@ -2,12 +2,12 @@ package dev.alexengrig.microservice.publication.text.controller;
 
 import dev.alexengrig.microservice.publication.text.service.PublicationTextService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +17,10 @@ public class PublicationTextController {
     private final PublicationTextService service;
 
     @PostMapping
-    public UUID publish(@RequestBody String text) {
-        return service.publish(text);
+    public ResponseEntity<String> publish(@RequestBody String text) {
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(service.publish(text).toString());
     }
 
 }
