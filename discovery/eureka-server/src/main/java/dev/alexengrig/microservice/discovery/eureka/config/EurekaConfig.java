7617f565990c2b0159da5bd9f6de95fd2e2c51dd
Grig Alex <alexengrigdev@gmail.com>
2023-01-06 07:00:55 +0300

    Create eureka server

---
+++ discovery/eureka-server/src/main/java/dev/alexengrig/microservice/discovery/eureka/config/EurekaConfig.java
@@ -0,0 +1,9 @@
+package dev.alexengrig.microservice.discovery.eureka.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaConfig {
+}
