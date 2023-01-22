5c880dce7a718eee3b8d48c34365ccb3116586fb
Grig Alex <alexengrigdev@gmail.com>
2023-01-22 08:08:19 +0300

    Create config server

---
+++ config/config-server/src/main/java/dev/alexengrig/microservice/config/server/config/ServerConfig.java
@@ -0,0 +1,9 @@
+package dev.alexengrig.microservice.config.server.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ServerConfig {
+}
