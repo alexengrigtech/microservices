f51574c8b2024f2638eda2a4919a8b5783130946
Grig Alex <alexengrigdev@gmail.com>
2023-01-27 01:54:55 +0300

    Fix class names

--- discovery/eureka-server/src/main/java/dev/alexengrig/microservice/discovery/eureka/EurekaServerApplication.java
+++ discovery/eureka-server/src/main/java/dev/alexengrig/microservice/discovery/eureka/EurekaServerApplication.java
@@ -2,12 +2,14 @@ package dev.alexengrig.microservice.discovery.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
